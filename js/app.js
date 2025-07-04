c883ce4995f9c87a8a70947ed5ce1dbc9c34b7ac
devadib <devwebadib@gmail.com>
2025-07-05 02:15:33 +0330

    add project

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector('.btn')
+const wrapper = document.querySelector('.heart-explosion')
+const img = document.querySelector('.image')
+btn.addEventListener('click',()=>{
+    wrapper.style.display ="block"
+    btn.remove()
+    img.style.display = "block"
+
+
+
+
+
+
+
+})
